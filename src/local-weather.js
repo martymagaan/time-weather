7b34cdc8186bc01e57793d27b93b1dd257c700b8
Marty <martymagaan@gmail.com>
2019-02-09 21:50:25 +0000

    feat: add wind direction

--- src/local-weather.js
+++ src/local-weather.js
@@ -7,6 +7,7 @@ const extractWeatherFromAPIData = apiData => (
 const formatWeatherData = data => ({
   'Temperature': `${data.temp_F} \xB0 F / ${data.temp_C} \xB0 C`,
   'Wind speed': `${data.windspeedMiles} mph / ${data.windspeedKmph} kmph`,
+  'Wind direction': `${data.winddir16Point}`,
   'Precipitation': `${data.precipMM} mm`,
   'Humidity': `${data.humidity} %`,
   'Pressure': `${data.pressure} mb`
