05a7a4237a5e1093858872e63198964eb6a9fb7e
Marty <martymagaan@gmail.com>
2019-02-09 20:33:52 +0000

    feat: extract and format requested data

--- src/search-location.js
+++ src/search-location.js
@@ -1,7 +1,21 @@
 const apiRequest = require('./api-request')
 
-const searchLocation = async location => (
+const extractLocationFromAPIData = apiData => (
+  apiData['search_api'] ? apiData['search_api']['result'][0] : null
+)
+
+const formatLocationName = data => (
+  data ? ({
+    area: `${data.areaName[0].value}`,
+    region: `${data.region[0].value}`,
+    country: `${data.country[0].value}`
+  }) : null
+)
+
+const searchLocation = location => (
   apiRequest('search', location)
+    .then(extractLocationFromAPIData)
+    .then(formatLocationName)
 )
 
 module.exports = searchLocation
