b875be8b798a1a6a9f27904446843ad7caf744cd
Marty <martymagaan@gmail.com>
2019-02-09 21:27:31 +0000

    feat: add request weather module

---
+++ src/local-weather.js
@@ -0,0 +1,22 @@
+const apiRequest = require('./api-request')
+
+const extractWeatherFromAPIData = apiData => (
+  apiData.data['current_condition'][0]
+)
+
+const formatWeatherData = data = ({
+  'Temperature': `${data.temp_F} \xB0 F / ${data.temp_C} \xB0 C`,
+  'Wind speed': `${data.windspeedMiles} mph / ${data.windspeedKmph} kmph`,
+  'Precipitation': `${data.precipMM} mm`,
+  'Humidity': `${data.humidity} %`,
+  'Pressure': `${data.pressure} mb`
+})
+
+const localWeather = location => (
+  apiRequest('weather', location)
+    .then(extractWeatherFromAPIData)
+    .then(formatWeather)
+)
+
+module.exports = localWeather
+
